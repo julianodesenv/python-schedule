4f091432682b1fc782e3bd6578ebe5abcedb8777
unknown <julianodesenv@gmail.com>
2021-10-21 11:15:22 -0300

    404 error handling

--- contacts/views.py
+++ contacts/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from .models import Contact
 
 
 def index(request):
     contacts = Contact.objects.all()
     return render(request, 'contacts/index.html', {'contacts': contacts})
+
+
+def show(request, contact_id):
+    contact = get_object_or_404(Contact, id=contact_id)
+    return render(request, 'contacts/show.html', {'contact': contact})
+    
